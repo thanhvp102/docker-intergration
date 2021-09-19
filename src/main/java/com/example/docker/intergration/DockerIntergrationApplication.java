8eb0be8e8eddb99ecbbd13d66b1c0c6cf4f7658f
unknown <thanhpham.khmt2@gmail.com>
2021-09-19 20:26:17 +0700

    first commit

---
+++ src/main/java/com/example/docker/intergration/DockerIntergrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker.intergration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerIntergrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerIntergrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/docker/intergration/DockerIntergrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker.intergration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerIntergrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
